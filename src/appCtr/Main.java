fbdd22e94b2aee75603d17aebbe476e8f3dd8df7
MaximePoulin899 <maxime.poulin.artiste@gmail.com>
2022-06-03 11:23:12 -0400

    second commit

--- src/appCtr/Main.java
+++ src/appCtr/Main.java
@@ -1,7 +1,17 @@
 package appCtr;
 
+import model.Bebay;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("hello mtf");
     }
+
+
+    Bebay babe = new Bebay("bibi", "21-01-2021");
+
+
+
+
 }
---
+++ src/model/Bebay.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Bebay {
+    private String nom;
+    private String dateBirth;
+
+    private int age;
+
+
+    public Bebay(String nom, String dateBirth, int age) {
+        this.nom = nom;
+        this.dateBirth = dateBirth;
+        this.age = age;
+    }
+
+    public Bebay(String nom, String dateBirth) {
+        this.nom = nom;
+        this.dateBirth = dateBirth;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(String dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+}
